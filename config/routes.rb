eb3064ea1b1eddc81cbfbb8449c0f1117427224a
basem909 <basemshams30@gmail.com>
2022-11-08 13:34:32 -0300

    Finish Room functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  resources :rooms do
+    resources :messages
+  end
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root to: "home#index"
+  get 'user/:id', to: 'users#show', as: 'user'
 end
